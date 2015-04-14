63e2e12aff0dac8baca95308277fd0c5c9ea3fa0
Maxime Emorine <maxime.emorine@hotmail.fr>
2015-04-14 14:59:19 +0200

    Edt OK

---
+++ src/Mmitv/FrontBundle/Resources/public/js/edt.js
@@ -0,0 +1,726 @@
+{
+  "MMI 1": {
+    "1": {
+      "1": {
+        "matiere": "M2108",
+        "libmatiere": "PPP",
+        "prof": "CHÉRAIN Olivier",
+        "salle": "H101",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "3": {
+        "matiere": "M2108",
+        "libmatiere": "PPP",
+        "prof": "MARTIN Martial",
+        "salle": "H104",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "5": {
+        "matiere": "M2106",
+        "libmatiere": "Communication, expression écrite et orale",
+        "prof": "BIDAUD Samuel",
+        "salle": "H103",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M2107",
+        "libmatiere": "Conduite de projet",
+        "prof": "LORENZI Emmanuel",
+        "salle": "H007",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M2107",
+        "libmatiere": "Conduite de projet",
+        "prof": "LORENZI Emmanuelle",
+        "salle": "H008",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "4": {
+      "1": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "BAUSER Thierry",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "ALART Jean-Charles",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M2204",
+        "libmatiere": "Services sur réseaux",
+        "prof": "GOMMERY Patrice",
+        "salle": "H205",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M2107",
+        "libmatiere": "Conduite de projet",
+        "prof": "LORENZI Emmanuelle",
+        "salle": "H007",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "5": {
+        "matiere": "M2107",
+        "libmatiere": "Conduite de projet",
+        "prof": "LORENZI Emmanuel",
+        "salle": "H008",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M2106",
+        "libmatiere": "Communication, expression écrite et orale",
+        "prof": "BIDAUD Samuel",
+        "salle": "H103",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M2108",
+        "libmatiere": "PPP",
+        "prof": "MARTIN Martial",
+        "salle": "H104",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      }
+    },
+    "7": {
+      "1": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "BAUSER Thierry",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "ALART Jean-Charles",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M2107",
+        "libmatiere": "Conduite de projet",
+        "prof": "LORENZI Emmanuelle",
+        "salle": "H007",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M2106",
+        "libmatiere": "Communication, expression écrite et orale",
+        "prof": "BIDAUD Samuel",
+        "salle": "H103",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "5": {
+        "matiere": "M2108",
+        "libmatiere": "PPP",
+        "prof": "CHÉRAIN Olivier",
+        "salle": "H111",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "7": {
+        "matiere": "M2201",
+        "libmatiere": "Culture scientifique et traitement de l'information",
+        "prof": "HUEZ Régis",
+        "salle": "E107",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "13": {
+      "1": {
+        "matiere": "M2109",
+        "libmatiere": "Environnement juridique, économique et mercatique des organisations",
+        "prof": "PETIT Michel",
+        "salle": "H104",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M2203",
+        "libmatiere": "Base de données",
+        "prof": "DARRAS Xavier",
+        "salle": "H006",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M2105",
+        "libmatiere": "Écriture pour les médias numériques",
+        "prof": "BIDAUD Samuel",
+        "salle": "H201",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "5": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "MEUZERET Fabrice",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "ALART Jean-Charles",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M2109",
+        "libmatiere": "Environnement juridique, économique et mercatique des organisations",
+        "prof": "LORENZI Emmanuelle",
+        "salle": "H101",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      }
+    },
+    "16": {
+      "1": {
+        "matiere": "M2203",
+        "libmatiere": "Base de données",
+        "prof": "DARRAS Xavier",
+        "salle": "H007",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M2109",
+        "libmatiere": "Environnement juridique, économique et mercatique des organisations",
+        "prof": "PETIT Michel",
+        "salle": "H104",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M2201",
+        "libmatiere": "Culture scientifique et traitement de l'information",
+        "prof": "HUEZ Régis",
+        "salle": "H006",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "5": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "MEUZERET Fabrice",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M2207",
+        "libmatiere": "Production audiovisuelle",
+        "prof": "ALART Jean-Charles",
+        "salle": "STUD",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M2203",
+        "libmatiere": "Base de données",
+        "prof": "ANNEBICQUE David",
+        "salle": "H008",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "19": {
+      "1": {
+        "matiere": "M2201",
+        "libmatiere": "Culture scientifique et traitement de l'information",
+        "prof": "HUEZ Régis",
+        "salle": "H006",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M2101",
+        "libmatiere": "Anglais",
+        "prof": "GROSDOIT-ARTUR Gilles",
+        "salle": "H101",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      }
+    }
+  },
+  "MMI 1D": {
+    "1": {
+      "1": {
+        "matiere": "M1202P",
+        "libmatiere": "Algorithmiques et Programmation",
+        "prof": "DARRAS Xavier",
+        "salle": "E110",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "4": {
+      "2": {
+        "matiere": "M1202P",
+        "libmatiere": "Algorithmiques et Programmation",
+        "prof": "DARRAS Xavier",
+        "salle": "E110",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "7": {
+      "1": {
+        "matiere": "M1103P",
+        "libmatiere": "Théories de l'information et de la communication",
+        "prof": "MARTIN Martial",
+        "salle": "H104",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      }
+    }
+  },
+  "MMI 2D": {
+    "1": {
+      "1": {
+        "matiere": "M3107P",
+        "libmatiere": "Conduite de projet",
+        "prof": "ONDERNARD Florent",
+        "salle": "H201",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      }
+    },
+    "4": {
+      "1": {
+        "matiere": "M3107P",
+        "libmatiere": "Conduite de projet",
+        "prof": "ONDERNARD Florent",
+        "salle": "H201",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M3206P",
+        "libmatiere": "Intégration web",
+        "prof": "THAï Tang Minh Nhat",
+        "salle": "H005",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "7": {
+      "1": {
+        "matiere": "M3206P",
+        "libmatiere": "Intégration web",
+        "prof": "THAï Tang Minh Nhat",
+        "salle": "H005",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M3107P",
+        "libmatiere": "Conduite de projet",
+        "prof": "ONDERNARD Florent",
+        "salle": "H201",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    }
+  },
+  "MMI 2": {
+    "1": {
+      "3": {
+        "matiere": "M41B2",
+        "libmatiere": "Technologie JS",
+        "prof": "LANDRÉ Jérôme",
+        "salle": "E109",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "5": {
+        "matiere": "M42B4",
+        "libmatiere": "Hébergement",
+        "prof": "GOMMERY Patrice",
+        "salle": "H205",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M42B3",
+        "libmatiere": "Webdesign et design mobile",
+        "prof": "VADOT Benjamin",
+        "salle": "H006",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "4": {
+      "1": {
+        "matiere": "M41C1",
+        "libmatiere": "Framework et POO avancé",
+        "prof": "ANNEBICQUE David",
+        "salle": "H111",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M4101",
+        "libmatiere": "Anglais",
+        "prof": "PIAT-MILLOT Sandrine",
+        "salle": "H105",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M4101",
+        "libmatiere": "Anglais",
+        "prof": "GROSDOIT-ARTUR Gilles",
+        "salle": "H101",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M42B3",
+        "libmatiere": "Webdesign et design mobile",
+        "prof": "VADOT Benjamin",
+        "salle": "H006",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M41B2",
+        "libmatiere": "Technologie JS",
+        "prof": "LANDRÉ Jérôme",
+        "salle": "E109",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "7": {
+      "1": {
+        "matiere": "M4101",
+        "libmatiere": "Anglais",
+        "prof": "GROSDOIT-ARTUR Gilles",
+        "salle": "H101",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M42B4",
+        "libmatiere": "Hébergement",
+        "prof": "GOMMERY Patrice",
+        "salle": "H205",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M41B2",
+        "libmatiere": "Technologie JS",
+        "prof": "LANDRÉ Jérôme",
+        "salle": "E109",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "5": {
+        "matiere": "M42B3",
+        "libmatiere": "Webdesign et design mobile",
+        "prof": "VADOT Benjamin",
+        "salle": "H006",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M4101",
+        "libmatiere": "Anglais",
+        "prof": "PIAT-MILLOT Sandrine",
+        "salle": "H105",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "13": {
+      "1": {
+        "matiere": "M41C1",
+        "libmatiere": "Framework et POO avancé",
+        "prof": "ANNEBICQUE David",
+        "salle": "H105",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "3": {
+        "matiere": "M42B3",
+        "libmatiere": "Webdesign et design mobile",
+        "prof": "VADOT Benjamin",
+        "salle": "H103",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      },
+      "5": {
+        "matiere": "M41B2",
+        "libmatiere": "Technologie JS",
+        "prof": "LANDRÉ Jérôme",
+        "salle": "H001",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "6": {
+        "matiere": "M42B4",
+        "libmatiere": "Hébergement",
+        "prof": "GOMMERY Patrice",
+        "salle": "H205",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    },
+    "16": {
+      "1": {
+        "matiere": "M42C4",
+        "libmatiere": "Serveur Windows",
+        "prof": "GOMMERY Patrice",
+        "salle": "H205",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M41C2",
+        "libmatiere": "Appli mobile et java",
+        "prof": "BAUSER Thierry",
+        "salle": "H005",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M41B2",
+        "libmatiere": "Technologie JS",
+        "prof": "LANDRÉ Jérôme",
+        "salle": "H001",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M42B3",
+        "libmatiere": "Webdesign et design mobile",
+        "prof": "VADOT Benjamin",
+        "salle": "H201",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "5": {
+        "matiere": "M4101",
+        "libmatiere": "Anglais",
+        "prof": "GROSDOIT-ARTUR Gilles",
+        "salle": "H101",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M42A4",
+        "libmatiere": "Sémiologie appliquée",
+        "prof": "BIDAUD Samuel",
+        "salle": "H103",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "td"
+      }
+    },
+    "19": {
+      "1": {
+        "matiere": "M41C2",
+        "libmatiere": "Appli mobile et java",
+        "prof": "BAUSER Thierry",
+        "salle": "H005",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "2": {
+        "matiere": "M42C4",
+        "libmatiere": "Serveur Windows",
+        "prof": "GOMMERY Patrice",
+        "salle": "H205",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "3": {
+        "matiere": "M42B3",
+        "libmatiere": "Webdesign et design mobile",
+        "prof": "VADOT Benjamin",
+        "salle": "H201",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "4": {
+        "matiere": "M41B2",
+        "libmatiere": "Technologie JS",
+        "prof": "LANDRÉ Jérôme",
+        "salle": "H001",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      },
+      "7": {
+        "matiere": "M42A4",
+        "libmatiere": "Sémiologie appliquée",
+        "prof": "BIDAUD Samuel",
+        "salle": "H103",
+        "evaluation": "N",
+        "texte": null,
+        "commentaire": null,
+        "type": "tp"
+      }
+    }
+  }
+}
--- src/Mmitv/FrontBundle/Resources/public/js/main.js
+++ src/Mmitv/FrontBundle/Resources/public/js/main.js
@@ -1,7 +1,207 @@
+function isset ()
+{
+
+  var a = arguments,
+    l = a.length,
+    i = 0,
+    undef;
+
+  if (l === 0)
+  {
+    throw new Error('Empty isset');
+  }
+
+  while (i !== l)
+  {
+    if (a[i] === undef || a[i] === null)
+    {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+
+
+// on récupere la date toutes les 5 secondes pour l'afficher dans la div #heure dans la nav
 $(function(){
 
-	$('#nav-trigger').on('click', function(){
+	date = new Date();
 
-		$('nav').toggleClass('nav-open');
-	})
+	h = date.getHours();
+    if(h<10)
+    {
+            h = "0"+h;
+    }
+    m = date.getMinutes();
+    if(m<10)
+    {
+            m = "0"+m;
+    }
+
+    resultat = h+':'+m;
+
+    $('nav #heure p').html(resultat);
+
+	setInterval(function() {
+
+		date = new Date();
+
+		h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+
+        resultat = h+':'+m;
+
+        $('nav #heure p').html(resultat);
+
+	}, 1000);
+
+
+	setInterval(function() {
+
+		$('#edt').html('<h1>Prochains cours</h1><div class="slider_edt"><ul></ul></div>');
+
+		// On récupere l'emploi du temps en json et on l'affiche en fonction de l'heure actuelle, à intervalle régulier // pour prendre en compte les modifications
+		$.getJSON("{{ asset('bundles/mmitvfront/js/edt.js')}}")
+			.done(function(json) {
+				console.log("JSON Edt OK");
+
+				var i=1;
+
+				var creneau = '1';
+
+				// On récupere la date pour calculer le creneau dans lequel on est
+
+				date = new Date();
+
+				h = date.getHours();
+		        if(h<10)
+		        {
+		                h = "0"+h;
+		        }
+
+		        m = date.getMinutes();
+		        if(m<10)
+		        {
+		                m = "0"+m;
+		        }
+
+		        // On calcule le créneau en cours
+		        if(h>=7 && h<9){
+		        	creneau= '1';
+		        }
+
+		        else if(h>=9 && h<10){
+		        	creneau= '4';
+		        }
+
+		        else if(h>=10 && h<12){
+		        	creneau= '7';
+		        }
+
+		        else if(h>=12 && h<15){
+		        	creneau= '13';
+		        }
+
+		        else if(h>=15 && h<16){
+		        	creneau= '16';
+		        }
+
+		        else if(h>=16 && h<18){
+		        	creneau= '19';
+		        }
+
+		        else if(h>=18 && h<19){
+		        	creneau= '22';
+		        }
+
+		        else{
+		        	creneau = '1';
+		        }
+		        //
+
+
+		        // On vérifie si il faut afficher ou non l'emploi du temps (5 minutes avant le cours et 5mn apres)
+
+		        // Si il est 55 ou 25, on affiche l'EDT
+		        if(h==7 || h==10 || h==13 || h==16 && m==54){
+		        	$('nav').attr('class', 'nav-open');
+		        }
+
+		        // Si il est 55 ou 25, on affiche l'EDT
+		        if(h==9 || h==12 || h==15 || h==18 && m==24){
+		        	$('nav').attr('class', 'nav-open');
+		        }
+
+				if(h==8 || h==11 || h==14 || h==17 && m==04){
+		        	$('nav').removeClass('nav-open');
+		        }
+
+		        if(h==9 || h==12 || h==15 || h==18 && m==34){
+		        	$('nav').removeClass('nav-open');
+		        }
+
+
+				for(var promo in json){
+
+					var edt = json[promo][creneau];
+
+					console.log(edt);
+
+					// on vérifie que la promo a bien cours dans ce créneau par sécurité
+					if(isset(edt)){
+
+						$('#edt .slider_edt>ul').append("<li><h2>"+promo+"</h2><ul></ul></li>");
+
+						for(var groupe in json[promo][creneau]){
+
+							var cours = json[promo][creneau][groupe];
+
+							$('#edt .slider_edt>ul>li:nth-child('+i+')>ul').append("<li><p>Groupe "+groupe+" | "+cours.matiere+" en "+cours.salle+"</p></li>");
+						}
+						i++;
+					}
+
+
+				}
+
+				// Après l'appel JSON on initialise le slider de l'edt
+				$('.slider_edt').unslider({
+					speed: 400, //temps de transition du slider (O = aucun effet de sliding)
+					delay: 10000, //changer ici le temps de changement entre chaque tweets
+					complete: function() {},
+					keys: false,
+					dots: false,
+					fluid: false
+				});
+
+			})
+			.fail(function(jqxhr, textStatus, error) {
+			    var err = textStatus + ", " + error;
+			    console.log( "JSON Edt Failed " + err );
+			    $('.slider_edt').html('Erreur d\'affichage de l\'emploi du temps');
+		});
+
+	}, 41600);
+
+
+	// Slider des tweets (bandeau inférieur)
+	$('.slider_tweets').unslider({
+		speed: 0, //temps de transition du slider (O = aucun effet de sliding)
+		delay: 8000, //changer ici le temps de changement entre chaque tweets
+		complete: function() {},
+		keys: false,
+		dots: false,
+		fluid: false
+	});
 });
+
