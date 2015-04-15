8da65c226fd95160ffa5a6b1b4445dbd07f227db
Maxime Emorine <maxime.emorine@hotmail.fr>
2015-04-15 09:58:03 +0200

    CRUD video ok

--- src/Mmitv/BackBundle/Controller/VideoController.php
+++ src/Mmitv/BackBundle/Controller/VideoController.php
@@ -9,13 +9,20 @@ use Symfony\Component\HttpFoundation\Request;
 
 class VideoController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction()
     {
 
     	// récupérer toutes les vidéos pour les afficher
     	$em = $this->getDoctrine()->getManager();
         $videos = $em->getRepository('MmitvBackBundle:videos')->findAll();
         // ---
+
+	    return $this->render('MmitvBackBundle:Video:index.html.twig', array('videos' => $videos));
+
+    }
+
+    public function addAction(Request $request)
+    {
 
        	// ajouter une video par le formulaire
         $video = new videos();
@@ -31,16 +38,40 @@ class VideoController extends Controller
 	      return $this->redirect($this->generateUrl('mmitv_back_video_index', array('id' => $video->getId())));
 	    }
 
-	    return $this->render('MmitvBackBundle:Video:index.html.twig', array(
+	    return $this->render('MmitvBackBundle:Video:add.html.twig', array(
 	      'form' => $form->createView(),
-	      'videos' => $videos
 	    ));
 	    // ---
-
     }
 
-    public function addAction()
+    public function editAction(Request $request, $id){
+
+    	$em = $this->getDoctrine()->getManager();
+        $video = $em->getRepository('MmitvBackBundle:videos')->find($id); //On va chercher l'tweet séléctionné
+        if (!$video) {
+            throw new NotFoundHttpException("Cette vidéo n'existe pas."); //Sécurité
+        }
+        $form = $this->createForm(new videosType(), $video); //On génère le formulaire
+
+        // Envoie dans le BDD
+        if ($form->handleRequest($request)->isValid()) {
+            $em->persist($video);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('mmitv_back_video_index'));
+        }
+
+        return $this->render('MmitvBackBundle:video:edit.html.twig', array('form' => $form->createView()));
+    }
+
+    public function deleteAction($id)
     {
-        return $this->render('MmitvBackBundle:Video:add.html.twig');
+
+		$em = $this->getDoctrine()->getManager();
+		$video = $em->getRepository('MmitvBackBundle:videos')->find($id);
+		$em->remove($video);
+		$em->flush();
+
+		return $this->redirect($this->generateUrl('mmitv_back_video_index'));
     }
 }
