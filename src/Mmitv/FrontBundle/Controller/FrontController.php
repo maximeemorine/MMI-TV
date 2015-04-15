3cd14bd57097d08e4c51be69e272a7935201e2e0
Kodoyosa <kodoyosa@gmail.com>
2015-04-15 10:27:45 +0200

    affichage
    Signed-off-by: Kodoyosa <kodoyosa@gmail.com>

--- src/Mmitv/FrontBundle/Controller/FrontController.php
+++ src/Mmitv/FrontBundle/Controller/FrontController.php
@@ -3,6 +3,10 @@
 namespace Mmitv\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 class FrontController extends Controller
 {
@@ -13,5 +17,24 @@ class FrontController extends Controller
         $tweets = $em->getRepository('MmitvBackBundle:tweet')->findAll();
 
         return $this->render('MmitvFrontBundle:Front:index.html.twig', array('tweets' => $tweets));
+    }
+
+    public function jsonAction()
+    {
+
+        $videos = $this->getDoctrine()
+            ->getRepository("MmitvBackBundle:videos")
+            ->findAll();
+
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $response = new Response($serializer->serialize($videos, "json"));
+        $response
+            ->headers
+            ->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
